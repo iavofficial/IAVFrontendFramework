1bf5f1994cee9b9e94eaa560dbdace7443883548
Krahn, Joerg Jonas (TG-D) <joerg.jonas.krahn@iav.de>
2021-03-11 11:07:33 +0000

    Update setup_npm.sh

--- ci_cd/scripts/setup_npm.sh
+++ ci_cd/scripts/setup_npm.sh
@@ -1,8 +1,3 @@
-if [[ ! -f package.json ]]; then
-  echo "No package.json found! A package.json file is required to publish a package to GitLab's NPM registry."
-  echo 'For more information, see https://docs.gitlab.com/ee/user/packages/npm_registry/#creating-a-project'
-  exit 1
-fi
 NPM_PACKAGE_NAME=$(node -p "require('../../package.json').name")
 NPM_PACKAGE_VERSION=$(node -p "require('../../package.json').version")
 npm config set cafile $IAV_CA_BUNDLE

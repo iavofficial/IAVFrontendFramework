ce0e105fe9a9bccf9017fea232d3caa49c27440f
Krahn, Joerg Jonas (TG-D) <joerg.jonas.krahn@iav.de>
2021-03-11 10:56:25 +0000

    Update setup_npm.sh

--- ci_cd/scripts/setup_npm.sh
+++ ci_cd/scripts/setup_npm.sh
@@ -3,8 +3,6 @@ if [[ ! -f package.json ]]; then
   echo 'For more information, see https://docs.gitlab.com/ee/user/packages/npm_registry/#creating-a-project'
   exit 1
 fi
-NPM_PACKAGE_NAME=$(node -p "require('./package.json').name")
-NPM_PACKAGE_VERSION=$(node -p "require('./package.json').version")
-npm config set cafile ./ci_cd/ca-bundle-including-iav_gitlab.crt
-
-echo SETUP
+NPM_PACKAGE_NAME=$(node -p "require('../../package.json').name")
+NPM_PACKAGE_VERSION=$(node -p "require('../../package.json').version")
+npm config set cafile $IAV_CA_BUNDLE

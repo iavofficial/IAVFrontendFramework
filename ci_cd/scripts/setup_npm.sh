77ecb50ad5448a361153bfe8f4b4c2490c7f9b8f
Krahn, Joerg Jonas (TG-D) <joerg.jonas.krahn@iav.de>
2021-03-11 11:25:34 +0000

    Update setup_npm.sh

--- ci_cd/scripts/setup_npm.sh
+++ ci_cd/scripts/setup_npm.sh
@@ -1,3 +1,3 @@
-NPM_PACKAGE_NAME=$(node -p "require('package.json').name")
-NPM_PACKAGE_VERSION=$(node -p "require('package.json').version")
+NPM_PACKAGE_NAME=$(node -p "require('./package.json').name")
+NPM_PACKAGE_VERSION=$(node -p "require('./package.json').version")
 npm config set cafile $IAV_CA_BUNDLE

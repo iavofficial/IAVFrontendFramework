5ca4e5aca2d03edf5fa75673172c1542843960a7
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-03-18 18:47:15 +0100

    Added hover effect with radium

--- src/components/Disatab.js
+++ src/components/Disatab.js
@@ -1,37 +1,38 @@
 import PropTypes from 'prop-types';
-import radium from 'radium';
+import Radium from 'radium';
 import { BLUE3 } from './Constants.js';
 import { Link } from 'react-router-dom';
 
 const Disatab = (props) => {
     const tabStyle = {
-        "minHeight": "70px",
-        "cursor": props.active || props.disabled ? "default" : "pointer",
-        "backgroundColor": props.active && !props.disabled ? BLUE3 : "white",
-        "color": props.active && !props.disabled ? "white" : "black",
-        "opacity": props.disabled ? 0.5 : 1,
-        ":hover" : {
-            "backgroundColor": BLUE3
+        minHeight: "70px",
+        cursor: props.active || props.disabled ? "default" : "pointer",
+        backgroundColor: props.active && !props.disabled ? BLUE3 : "white",
+        color: props.active && !props.disabled ? "white" : "black",
+        opacity: props.disabled ? 0.5 : 1,
+        marginBottom: "1px",
+        ':hover': {
+            backgroundColor: props.disabled? "white" : BLUE3
         }
     }
-    let tab = radium(
+    const tab = (
         <div className="p-d-flex p-align-center" style={tabStyle}>
             <span className="p-d-flex p-align-center" style={{ "marginLeft": "5%" }}>
                 <img src={props.active && !props.disabled ? props.selectedIcon : props.deselectedIcon}></img>
                 <span style={{ "fontWeight": "bold" }}>{props.name}</span>
             </span>
         </div>
-    )
+    );
     return (
         props.disabled ? tab :
             <Link style={{ "textDecoration": "none" }} to={props.to}>
                 {tab}
             </Link>
-    )
+    );
 }
 
 Disatab.propTypes = {
     props: PropTypes.object
 }
 
-export default Disatab;
+export default Radium(Disatab);

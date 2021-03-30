24fdec5f60a32c42b51201ccc109c42da0d94450
Krahn, Joerg Jonas (TG-D) <joerg.jonas.krahn@iav.de>
2021-03-30 13:50:17 +0000

    Update create_lib.sh

--- ci_cd/scripts/create_lib.sh
+++ ci_cd/scripts/create_lib.sh
@@ -1,4 +1,4 @@
-rm -R ./lib && mkdir ./lib;
+mkdir ./lib;
 cp -R ./src/assets ./lib/assets;
 npx babel ./src/components --out-dir ./lib/components --copy-files;
 npx babel ./src/links --out-dir . --copy-files;

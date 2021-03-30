4364528881d7e291baad4a6ef7a53abd9ef0eb7f
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-03-30 12:34:08 +0200

    Update setup_npm.sh

--- ci_cd/scripts/setup_npm.sh
+++ ci_cd/scripts/setup_npm.sh
@@ -1,3 +1,4 @@
-npm config set cafile $IAV_CA_BUNDLE;
 npm config set proxy $HTTP_PROXY;
+#npm config set registry http://registry.npmjs.org/;
 npm install --only=dev;
+npm config set cafile $IAV_CA_BUNDLE;

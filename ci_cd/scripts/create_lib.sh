6056c112b1bd9a0aca31451d4271b695dfc0c883
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-03-30 11:57:43 +0200

    Update create_lib.sh

--- ci_cd/scripts/create_lib.sh
+++ ci_cd/scripts/create_lib.sh
@@ -1,3 +1,3 @@
 rm -R ./lib;
-mkdir -p ./lib/assets && cp -R ./src/assets;
+mkdir -p ./lib/assets && cp -R ./src/assets ./lib/assets;
 npx babel ./src/components --out-dir ./lib/components --copy-files;

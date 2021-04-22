677d9184ea622e0f597323da0b4d6f0f8fe35186
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-04-22 17:53:48 +0200

    updated build scripts

--- scripts/ci_cd/create_lib.sh
+++ scripts/ci_cd/create_lib.sh
@@ -5,8 +5,8 @@ npx tsc;
 
 if [ $? = 0 ]
 then
-    npx babel ./src/components --out-dir ./lib/components --copy-files;
-    npx babel ./src/contexts --out-dir ./lib/contexts --copy-files;
+    npx babel ./src/components --out-dir ./lib/components --copy-files --extensions .ts,.tsx;
+    npx babel ./src/contexts --out-dir ./lib/contexts --copy-files --extensions .ts,.tsx;
     npx babel ./src/links --out-dir . --copy-files;
     npx tsc --p ./generate_decl_tsconfig.json;
 else
--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -1,11 +1,7 @@
 rmdir /S /q lib
 Xcopy src\assets lib\assets /i
 
-npx tsc
-
-if %errorlevel%==0 (
-    npx babel src\components --out-dir lib\components --copy-files --extensions .ts,.tsx &^
-    npx babel src\contexts --out-dir lib\contexts --copy-files --extensions .ts,.tsx &^
-    npx babel src\links --out-dir lib --copy-files &&^
-    npx tsc --p ./generate_decl_tsconfig.json
-)
+npx babel src\components --out-dir lib\components --copy-files --extensions .ts,.tsx &^
+npx babel src\contexts --out-dir lib\contexts --copy-files --extensions .ts,.tsx &^
+npx babel src\links --out-dir lib --copy-files &&^
+npx tsc --p ./generate_decl_tsconfig.json

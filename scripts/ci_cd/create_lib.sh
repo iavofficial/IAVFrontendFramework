3cafa34972c5c8fd12c7f9b694f5c73d9ed5b176
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-04-29 17:18:20 +0200

    Update create_lib.sh

--- scripts/ci_cd/create_lib.sh
+++ scripts/ci_cd/create_lib.sh
@@ -5,8 +5,8 @@ npx tsc;
 if [ $? = 0 ]
 then
     npx babel ./src/lib --out-dir ./lib/lib --extensions .ts,.tsx --copy-files;
-    npx babel ./src/links --out-dir ./lib --extensions .ts,.tsx --copy-files
-    cp package.json ./lib
+    npx babel ./src/links --out-dir ./lib --extensions .ts,.tsx --copy-files;
+    cp package.json ./lib;
 else
     exit 1;
 fi

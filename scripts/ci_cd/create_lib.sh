4b1992a0d9ba49b21d4d0b6becfcd56b66415b45
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-04-29 17:44:37 +0200

    updated release of package

--- scripts/ci_cd/create_lib.sh
+++ scripts/ci_cd/create_lib.sh
@@ -6,7 +6,7 @@ if [ $? = 0 ]
 then
     npx babel ./src/lib --out-dir ./lib/lib --extensions .ts,.tsx --copy-files;
     npx babel ./src/links --out-dir ./lib --extensions .ts,.tsx --copy-files;
-    cp package.json ./lib;
+    cp ./package.json ./lib;
 else
     exit 1;
 fi

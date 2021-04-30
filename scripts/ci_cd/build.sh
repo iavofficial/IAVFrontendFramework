de1f5d45e0882e8ba409a4d4aa0c49e96dd6f03b
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-04-30 12:39:12 +0200

    fixed bug (wrong build folder)

--- scripts/ci_cd/build.sh
+++ scripts/ci_cd/build.sh
@@ -4,11 +4,8 @@ npx tsc;
 
 if [ $? = 0 ]
 then
-    npx babel ./src/build --out-dir ./build/build --extensions .ts,.tsx --copy-files;
+    npx babel ./src/lib --out-dir ./build/lib --extensions .ts,.tsx --copy-files;
     npx babel ./src/links --out-dir ./build --extensions .ts,.tsx --copy-files;
-    cp ./package.json ./build;
-    cp ./.npmrc ./build;
-    cp ./README.md ./build;
 else
     exit 1;
 fi
--- scripts/windows/build_devmode.bat
+++ scripts/windows/build_devmode.bat
@@ -1,5 +1,5 @@
 rmdir /S /q build
 
-start /b npx babel ./src/build --out-dir ./build/build --extensions .ts,.tsx --watch --copy-files
+start /b npx babel ./src/lib --out-dir ./build/lib --extensions .ts,.tsx --watch --copy-files
 start /b npx babel ./src/links --out-dir ./build --extensions .ts,.tsx --watch --copy-files
 start /b npx tsc --watch

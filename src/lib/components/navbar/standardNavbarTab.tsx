2ad61e2f1c92ac910abb92dbf341303f297a2945
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-05-11 17:31:21 +0200

    Fixed bug: image wouldn't be changed if hovering over it

--- src/lib/components/navbar/standardNavbarTab.tsx
+++ src/lib/components/navbar/standardNavbarTab.tsx
@@ -1,35 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import Radium from "radium";
 import { Link } from "react-router-dom";
 
 import { BLUE1, GRAY2, DISATABHEIGHT } from "../constants";
-import { navbarTab } from "./navbarTab";
+import { navbarTabProps } from "./navbarTab";
 
-const StandardNavbarTabUnprocessed: navbarTab = (props) => {
-    const tabStyle = {
-        height: DISATABHEIGHT,
-        cursor: props.active || props.disabled ? "default" : "pointer",
-        backgroundColor: props.active && !props.disabled ? BLUE1 : "white",
-        color: props.active && !props.disabled ? "white" : "black",
-        opacity: props.disabled ? 0.5 : 1,
-        ":hover": {
-            backgroundColor: props.disabled ? "white" : BLUE1
+interface State {
+    hovering: Boolean
+}
+
+class StandardNavbarTabUnprocessed extends Component<navbarTabProps, State> {
+
+    constructor(props: navbarTabProps) {
+        super(props);
+        this.state = {
+            hovering: false
         }
     }
-    const tab = (
-        <div style={{ borderStyle: "solid", borderWidth: "0px 0px 1px 0px", borderColor: GRAY2 }}>
-            <div className="p-d-flex p-align-center" style={tabStyle}>
-                <img src={props.active && !props.disabled ? props.selectedIcon.valueOf() : props.deselectedIcon.valueOf()} style={{ marginLeft: "5%" }} alt="" />
-                <span style={{ fontWeight: "bold" }}>{props.name}</span>
+
+    setHovering = (hovering: Boolean) => {
+        this.setState({ hovering: hovering });
+    }
+
+    render() {
+        let tabStyle = {
+            height: DISATABHEIGHT,
+            cursor: this.props.active || this.props.disabled ? "default" : "pointer",
+            backgroundColor: this.props.active && !this.props.disabled ? BLUE1 : "white",
+            color: this.props.active && !this.props.disabled ? "white" : "black",
+            opacity: this.props.disabled ? 0.5 : 1,
+            ":hover": {
+                backgroundColor: this.props.disabled ? "white" : BLUE1
+            }
+        }
+        const tab = (
+            <div style={{ borderStyle: "solid", borderWidth: "0px 0px 1px 0px", borderColor: GRAY2 }}
+                onMouseEnter={() => this.setHovering(true)} onMouseLeave={() => this.setHovering(false)}>
+                <div className="p-d-flex p-align-center" style={tabStyle}>
+                    <img src={(this.props.active || this.state.hovering) && !this.props.disabled ? this.props.selectedIcon.valueOf() : this.props.deselectedIcon.valueOf()}
+                        style={{ marginLeft: "5%" }} alt="" />
+                    <span style={{ fontWeight: "bold" }}>{this.props.name}</span>
+                </div>
             </div>
-        </div>
-    );
-    return (
-        props.disabled ? tab :
-            <Link style={{ textDecoration: "none" }} to={props.to.valueOf()}>
-                {tab}
-            </Link>
-    );
+        );
+        return (
+            this.props.disabled ? tab :
+                <Link style={{ textDecoration: "none" }} to={this.props.to.valueOf()}>
+                    {tab}
+                </Link>
+        );
+    }
 }
 
 export const StandardNavbarTab = Radium(StandardNavbarTabUnprocessed);

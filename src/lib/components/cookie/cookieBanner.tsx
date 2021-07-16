150993ece42a3b3b4cedc594e9cdea54779f2cca
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-07-16 15:57:06 +0200

    Update cookieBanner.tsx

--- src/lib/components/cookie/cookieBanner.tsx
+++ src/lib/components/cookie/cookieBanner.tsx
@@ -7,9 +7,6 @@ import { acceptedCookiesName } from "./cookieHandler";
 
 interface State {
     visible: boolean;
-    icon: string;
-    iconColor: string;
-    message: string;
 }
 
 export class CookieBanner extends Component<any, State> {
@@ -17,23 +14,14 @@ export class CookieBanner extends Component<any, State> {
         super(props);
         this.state = {
             visible: true,
-            icon: "pi pi-info-circle",
-            iconColor: "black",
-            message: "This website needs you to allow cookies for proper functionality."
         }
-        this.acceptCookies = this.acceptCookies.bind(this);
-        this.rejectCookies = this.rejectCookies.bind(this);
     }
 
-    acceptCookies() {
+    acceptCookies = () => {
         let exprireDate = new Date();
         exprireDate.setUTCFullYear(exprireDate.getUTCFullYear() + 1);
         document.cookie = acceptedCookiesName + "=true; expires=" + exprireDate.toUTCString();
         this.setState({ visible: false });
-    }
-
-    rejectCookies() {
-        this.setState({ visible: true, icon: "pi pi-times-circle", iconColor: RED, message: "You have to accept cookies. Otherwise you can't use this website." });
     }
 
     // Dialog has to have the onHide property. Otherwise the typescript compiler will throw an error.
@@ -42,14 +30,12 @@ export class CookieBanner extends Component<any, State> {
             <Dialog header="This website uses cookies." position={"bottom"} visible={this.state.visible} modal dismissableMask={false} closable={false} onHide={() => ""}
                 footer={
                     <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                        <Button icon="pi pi-times" className="p-button-secondary" label="Reject cookies" onClick={this.rejectCookies}
-                            style={{ backgroundColor: GRAY3, border: "none" }} />
                         <Button icon="pi pi-check" label="Allow cookies" onClick={this.acceptCookies} style={{ backgroundColor: BLUE1, border: "none" }} />
                     </div>
                 }>
                 <div style={{ display: "flex", alignItems: "center" }}>
-                    <span className={this.state.icon.valueOf()} style={{ marginRight: "10px", fontSize: "xx-large", color: this.state.iconColor.valueOf() }} />
-                    <span>{this.state.message}</span>
+                    <span className={"pi pi-info-circle"} style={{ marginRight: "10px", fontSize: "xx-large", color: "black" }} />
+                    <span>This website needs you to allow cookies for proper functionality.</span>
                 </div>
             </Dialog>
         );

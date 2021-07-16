5a42bee7ca3aba517aa99e7549e9d9306f973b3d
IAVGROUP\j01krahn <joerg.jonas.krahn@iav.de>
2021-07-16 16:38:01 +0200

    Update thirdExampleComponent.tsx

--- example/src/components/thirdExampleComponent.tsx
+++ example/src/components/thirdExampleComponent.tsx
@@ -3,16 +3,13 @@ import { FirstExampleContext } from "../contexts/FirstExampleContext";
 import { Content } from "disa-framework/content";
 import { useTranslation } from "react-i18next";
 
-import { initI18next } from "disa-framework/i18n"
-
 export const ThirdExampleComponent = () => {
     const context = useContext(FirstExampleContext);
     const { t, i18n } = useTranslation();
-    initI18next();
     return (
         <Content contentElements={context ? context.contentTabs : []}>
             <div>Example data <b>global</b> context: {context ? context.exampleData : ""}</div>
-            <div>Example translation: {t("test")}</div>
+            <div>Example translation: {t("Imprint")}</div>
         </Content>
     );
 }

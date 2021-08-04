13f23a1d6efccbe00f5d58710ee264879c7a6fad
Krahn, Joerg Jonas (TG-D) <joerg.jonas.krahn@iav.de>
2021-08-04 21:07:07 +0200

    Update build_package_json.js

--- scripts/ci_cd/build_package_json.js
+++ scripts/ci_cd/build_package_json.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 fs.readFile("./package.json", (err, data) => {
     if (err) throw err;
     let packageJsonObj = JSON.parse(data);
-    packageJsonObj.name = "abc/" + packageJsonObj.name;
+    packageJsonObj.name = "@" + process.env.REPO_ROOT_NAMESPACE + "/" + packageJsonObj.name;
     fs.writeFile("./package.json", JSON.stringify(packageJsonObj), (err) => {
         if (err) throw err;
         console.log("The file has been saved!");

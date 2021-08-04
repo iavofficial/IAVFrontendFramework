96c959f9c4988729b0220b9bb0d42ccb8eba67d2
Krahn, Joerg Jonas (TG-D) <joerg.jonas.krahn@iav.de>
2021-08-04 21:14:41 +0200

    removed copying of package.json

--- scripts/ci_cd/build.sh
+++ scripts/ci_cd/build.sh
@@ -6,7 +6,6 @@ if [ $? = 0 ]
 then
     npx babel ./src/lib --out-dir ./build/lib --extensions .ts,.tsx --copy-files;
     npx babel ./src/links --out-dir ./build --extensions .ts,.tsx --copy-files;
-    cp ./package.json ./build;
     cp ./.npmrc ./build;
     cp ./README.md ./build;
 else

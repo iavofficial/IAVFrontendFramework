016a5402a24a773f93e050df1bd8fd0641b812cc
Krahn, Joerg Jonas (TG-D) <joerg.jonas.krahn@iav.de>
2021-08-04 11:47:26 +0200

    Update setup_npm.sh

--- scripts/ci_cd/setup_npm.sh
+++
@@ -1,7 +0,0 @@
-npm config set proxy $HTTP_PROXY;
-echo "Printing CA variable";
-echo $IAV_CA_BUNDLE;
-echo "Should have printed";
-npm install --only=dev;
-#npm config set cafile $IAV_CA_BUNDLE;
-echo "successfull"

baf91366ead1999da63e0d8371b7fc2c047dd756
Krahn, Joerg Jonas (TG-D) <joerg.jonas.krahn@iav.de>
2021-08-16 18:12:21 +0200

    added config entries

--- scripts/ci_cd/configure_npm.sh
+++ scripts/ci_cd/configure_npm.sh
@@ -1,1 +1,4 @@
 npm config set proxy $HTTP_PROXY;
+
+npm config set "@disa:registry" "${REPO_PROTOCOL}://${REPO_URL}/api/v4/projects/${REPO_PROJECT_ID}/packages/npm/";
+npm config set "//${REPO_URL}/api/v4/projects/${REPO_PROJECT_ID}/packages/npm/:_authToken" "${REPO_DEPLOY_FRAMEWORK_AUTH_TOKEN}";

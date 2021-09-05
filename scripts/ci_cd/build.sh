b27f228dbd0bddec144bde3096cb25df4f7da378
Jörg Jonas Krahn <joerg.jonas.krahn@iav.de>
2021-09-05 23:10:52 +0200

    pipeline optimizations

--- scripts/ci_cd/build.sh
+++ scripts/ci_cd/build.sh
@@ -7,7 +7,6 @@ then
     npx babel ./src/lib --out-dir ./build/lib --extensions .ts,.tsx --copy-files;
     npx babel ./src/links --out-dir ./build --extensions .ts,.tsx --copy-files;
     cp ./package.json ./build
-    cp ./.npmrc ./build;
     cp ./README.md ./build;
 else
     exit 1;
--- scripts/windows/devmode_build.bat
+++ scripts/windows/devmode_build.bat
@@ -2,11 +2,8 @@ rmdir /S /q build
 mkdir build
 
 start /b npx babel ./src/lib --out-dir ./build/lib --extensions .ts,.tsx --watch --copy-files
-start /b npx babel ./src/links --out-dir ./build --extensions .ts,.tsx --watch --copy-files
+start /b npx babel ./src/links --out-dir ./build --watch --copy-files
 
-REM The package.json file is copied only once.
-copy ./package.json ./build
-
-start /b npx babel ./package.json --out-dir ./build --extensions .ts,.tsx --watch --copy-files
+start /b npx babel ./package.json --out-dir ./build --watch --copy-files
 
 start /b npx tsc --watch

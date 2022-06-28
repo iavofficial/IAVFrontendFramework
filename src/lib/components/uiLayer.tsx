b0535ce2272a9a7f1e8b324cb68f43efe9248222
Jörg Jonas Krahn <joerg.jonas.krahn@iav.de>
2022-06-28 16:46:40 +0200

    improved structure of UILayer and replaced declarative navigating by imperative navigating

--- src/lib/components/uiLayer.tsx
+++ src/lib/components/uiLayer.tsx
@@ -8,7 +8,8 @@ import {
     Route,
     useLocation,
     Routes,
-    Navigate
+    Navigate,
+    useNavigate
 } from 'react-router-dom';
 import './css/constants.css';
 import './css/disaPage.css';
@@ -24,6 +25,14 @@ import {TabAndContentWrapper} from './navbar/wrapper/tabAndContentWrapper';
 import {MenuOptions} from './navbar/menu';
 import {AuthenticationViewProps} from './authentication/aws/authenticationView';
 
+interface HeaderOptions {
+    reactElementRight?: ReactElement;
+    reactElementLeft?: ReactElement;
+    letteringElementLeft?: string;
+    hideLeft?: boolean;
+    hideRight?: boolean;
+}
+
 export interface Props {
     tabAndContentWrappers: TabAndContentWrapper[];
     startingPoint: string;
@@ -31,54 +40,34 @@ export interface Props {
     authenticationView?: React.ComponentType<AuthenticationViewProps & any>;
     documentsComponent?: React.ComponentType<any>;
     documentsLabelKey?: string;
-    headerOptions?: {
-        reactElementRight?: ReactElement;
-        reactElementLeft?: ReactElement;
-        letteringElementLeft?: string;
-        hideLeft?: boolean;
-        hideRight?: boolean;
-    };
+    headerOptions?: HeaderOptions;
 }
 
 // TODO: The creation of the components DefaultImprint, RSMView and Redirector inside UILayer may cause a problem.
 // Because the components are recreated every render, the Routes will get new components every render. This may cause a rerender of
 // all components which are encapsulated in this layer.
 export const UILayer = (props: Props) => {
-        const authContext = useContext(AuthContext);
-        const AuthenticationView = props.authenticationView ? props.authenticationView : BasicAuthenticationView;
+    const authContext = useContext(AuthContext);
+    const AuthenticationView = props.authenticationView ? props.authenticationView : BasicAuthenticationView;
 
-        const DefaultImprint = () => (
-            <div
-                className='p-d-flex'
-                style={{
-                    height: '100%',
-                    width: '100%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
-                <Imprint/>
-            </div>
-        );
-
-        // className="p-d-flex p-flex-column"
-        const RSMView = () => (
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                height: '100%',
-                bottom: '0'
-            }}>
-                <div style={{flex: '0 0 auto'}}>
-                    <DisaHeader headerOptions={props.headerOptions}/>
-                </div>
-                <div style={{display: 'flex', flex: '1 1 auto', overflow: 'auto'}}>
-                    <Navbar
-                        tabAndContentWrappers={props.tabAndContentWrappers}
-                        menuOptions={props.menuOptions}
-                        documentsLabelKey={props.documentsLabelKey}
+    return (
+        <>
+            <CookieBanner/>
+            <Router>
+                <Redirector
+                    startingPoint={props.startingPoint}
+                />
+                <Routes>
+                    <Route
+                        path='/login'
+                        element={
+                            <AuthenticationView
+                                documentsLabelKey={props.documentsLabelKey}
+                                headerOptions={props.headerOptions}
+                            />
+                        }
                     />
-                    <Routes>
-                        {props.tabAndContentWrappers.map((wrapper) => wrapper.getRoutes())}
+                    {!authContext?.hasAuthenticated() && (
                         <Route
                             path='/documents'
                             element={
@@ -88,57 +77,104 @@ export const UILayer = (props: Props) => {
                                     <DefaultImprint/>
                             }
                         />
-                    </Routes>
-                </div>
-            </div>
-        );
-
-        const Redirector = () => {
-            const location = useLocation();
-
-            if (!authContext?.hasAuthenticated()) {
-                if (location.pathname !== "/documents" && location.pathname !== "/login") {
-                    return <Navigate to="/login"/>;
-                }
-            } else {
-                if (location.pathname === "/login") {
-                    return <Navigate to={props.startingPoint.valueOf()}/>;
-                }
-            }
-
-            return <React.Fragment/>;
-        };
-
-        return (
-            <>
-                <CookieBanner/>
-                <Router>
-                    <Redirector/>
-                    <Routes>
-                        <Route
-                            path='/login'
-                            element={
-                                <AuthenticationView
-                                    documentsLabelKey={props.documentsLabelKey}
-                                    headerOptions={props.headerOptions}
-                                />
-                            }
+                    )}
+                    <Route path='/*' element={
+                        <RSMView
+                            headerOptions={props.headerOptions}
+                            tabAndContentWrappers={props.tabAndContentWrappers}
+                            menuOptions={props.menuOptions}
+                            documentsLabelKey={props.documentsLabelKey}
+                            documentsComponent={props.documentsComponent}
                         />
-                        {!authContext?.hasAuthenticated() && (
-                            <Route
-                                path='/documents'
-                                element={
-                                    props.documentsComponent ?
-                                        <props.documentsComponent/>
-                                        :
-                                        <DefaultImprint/>
-                                }
-                            />
-                        )}
-                        <Route path='/*' element={<RSMView/>}/>
-                    </Routes>
-                </Router>
-            </>
-        );
-    }
-;
+                    }/>
+                </Routes>
+            </Router>
+        </>
+    );
+};
+
+interface RedirectorProps {
+    startingPoint: string;
+}
+
+/**
+ * This component is needed because the useLocation hook can only be used inside a Router-component
+ * environment.
+ * @param props
+ * @constructor
+ */
+const Redirector = (props: RedirectorProps) => {
+    const authContext = useContext(AuthContext);
+    const userIsAuthenticated = authContext!.hasAuthenticated();
+    const currentPath = useLocation().pathname;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log("effect");
+        if (!userIsAuthenticated) {
+            if (currentPath !== "/documents" && currentPath !== "/login") {
+                navigate("/login");
+            }
+        } else {
+            if (currentPath === "/login") {
+                navigate(props.startingPoint.valueOf());
+            }
+        }
+    }, [currentPath, userIsAuthenticated, navigate]);
+
+    return <React.Fragment/>;
+}
+
+interface RSMViewProps {
+    tabAndContentWrappers: TabAndContentWrapper[];
+    menuOptions?: MenuOptions;
+    documentsComponent?: React.ComponentType<any>;
+    documentsLabelKey?: string;
+    headerOptions?: HeaderOptions;
+}
+
+// className="p-d-flex p-flex-column"
+const RSMView = (props: RSMViewProps) => (
+    <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        bottom: '0'
+    }}>
+        <div style={{flex: '0 0 auto'}}>
+            <DisaHeader headerOptions={props.headerOptions}/>
+        </div>
+        <div style={{display: 'flex', flex: '1 1 auto', overflow: 'auto'}}>
+            <Navbar
+                tabAndContentWrappers={props.tabAndContentWrappers}
+                menuOptions={props.menuOptions}
+                documentsLabelKey={props.documentsLabelKey}
+            />
+            <Routes>
+                {props.tabAndContentWrappers.map((wrapper) => wrapper.getRoutes())}
+                <Route
+                    path='/documents'
+                    element={
+                        props.documentsComponent ?
+                            <props.documentsComponent/>
+                            :
+                            <DefaultImprint/>
+                    }
+                />
+            </Routes>
+        </div>
+    </div>
+);
+
+const DefaultImprint = () => (
+    <div
+        className='p-d-flex'
+        style={{
+            height: '100%',
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+        <Imprint/>
+    </div>
+);

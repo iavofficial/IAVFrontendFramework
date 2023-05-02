2686d2e0a853c7824e6c760ad103d8666e989ab5
Tuerk, Oliver (TT-T) <oliver.tuerk@iav.de>
2023-05-02 14:32:55 +0200

    Trying to fix CI/CD timeout problem with artifactory...

--- scripts/ci_cd/configure_npm.sh
+++ scripts/ci_cd/configure_npm.sh
@@ -2,6 +2,7 @@ npm config set "@disa:registry" "${REPO_PROTOCOL}://${REPO_URL}/api/v4/projects/
 npm config set "//${REPO_URL}/api/v4/projects/${REPO_PROJECT_ID}/packages/npm/:_authToken" "${REPO_DEPLOY_FRAMEWORK_AUTH_TOKEN}";
 
 # npm config set registry="http://registry.npmjs.org/"
+npm config set registry https://artifactory.iavgroup.local/artifactory/api/npm/npm-remote/
 
 # npm config rm http-proxy
 # npm config rm https-proxy

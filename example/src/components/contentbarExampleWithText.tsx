653e4bfc7c47f6620f083ddd37ded722b2ebb013
Daniel Helfenstein <daniel.helfenstein@iav.de>
2023-06-01 17:24:26 +0200

    implementing changes

---
+++ example/src/components/contentbarExampleWithText.tsx
@@ -0,0 +1,49 @@
+import { CellPaddings, ContentCell } from 'disa-framework/contentCell';
+import React from 'react';
+
+export interface Props {
+  exampleText: string;
+}
+
+export const ContentbarExampleWithText = (props: Props) => {
+  return (
+    <>
+      <div className={'col-8 grid grid-nogutter'}>
+        <ContentCell colWidth={6} paddings={CellPaddings.FULL}>
+          <span>First row left</span>
+          <h2>{props.exampleText}</h2>
+          <h3>Contentbar with default tabelements</h3>
+          <div></div>
+        </ContentCell>
+        <ContentCell colWidth={6} paddings={CellPaddings.VERT_RIGHT}>
+          <span>First row center</span>
+        </ContentCell>
+        <ContentCell paddings={CellPaddings.BOT_HOR}>
+          <span>Second row left</span>
+        </ContentCell>
+        <ContentCell paddings={CellPaddings.BOT_RIGHT}>
+          <span>Second row center left</span>
+        </ContentCell>
+        <ContentCell paddings={CellPaddings.BOT_RIGHT}>
+          <span>Second row center</span>
+        </ContentCell>
+        <ContentCell paddings={CellPaddings.BOT_RIGHT}>
+          <span>Second row center right</span>
+        </ContentCell>
+        <ContentCell paddings={CellPaddings.BOT_RIGHT}>
+          <span>Second row right</span>
+        </ContentCell>
+        <ContentCell colWidth={12} paddings={CellPaddings.BOT_HOR}>
+          <span>Third row</span>
+        </ContentCell>
+      </div>
+      <ContentCell
+        paddings={CellPaddings.VERT_RIGHT}
+        colWidth={4}
+        clearStyle={true}
+      >
+        <span>Right with cleared style</span>
+      </ContentCell>
+    </>
+  );
+};
--- example/src/components/exampleComponent1.tsx
+++ example/src/components/exampleComponent1.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { CellPaddings, ContentCell } from 'disa-framework/contentCell';
 import { Content, LayoutBehaviour } from 'disa-framework/content';
+import { generateHashOfLength } from 'disa-framework/hash';
+import { ContentbarExampleWithText } from './contentbarExampleWithText';
 import { BasicContentbarWrapper } from 'disa-framework/basicContentbarWrapper';
 
 export const ExampleComponent1 = () => {
@@ -11,69 +13,69 @@ export const ExampleComponent1 = () => {
   };
 
   let exampleArray = [
-    // new BasicContentbarWrapper(
-    //   'test123',
-    //   'car123',
-    //   selectedId,
-    //   false,
-    //   setSelectedId,
-    //   onCloseElement,
-    //   <ContentbarExampleWithText exampleText="car123" />
-    // ),
-    // new BasicContentbarWrapper(
-    //   'test124',
-    //   'car124',
-    //   selectedId,
-    //   false,
-    //   setSelectedId,
-    //   onCloseElement,
-    //   <ContentbarExampleWithText exampleText="car124" />
-    // ),
-    // new BasicContentbarWrapper(
-    //   'test125',
-    //   'car125',
-    //   selectedId,
-    //   true,
-    //   setSelectedId,
-    //   onCloseElement,
-    //   <ContentbarExampleWithText exampleText="car125" />
-    // ),
-    // new BasicContentbarWrapper(
-    //   'test126',
-    //   'car126',
-    //   selectedId,
-    //   true,
-    //   setSelectedId,
-    //   onCloseElement,
-    //   <ContentbarExampleWithText exampleText="car126" />
-    // ),
-    // new BasicContentbarWrapper(
-    //   'test127',
-    //   'car127',
-    //   selectedId,
-    //   true,
-    //   setSelectedId,
-    //   onCloseElement,
-    //   <ContentbarExampleWithText exampleText="car127" />
-    // ),
-    // new BasicContentbarWrapper(
-    //   'test128',
-    //   'car128',
-    //   selectedId,
-    //   true,
-    //   setSelectedId,
-    //   onCloseElement,
-    //   <ContentbarExampleWithText exampleText="car128" />
-    // ),
-    // new BasicContentbarWrapper(
-    //   'test129',
-    //   'car129',
-    //   selectedId,
-    //   true,
-    //   setSelectedId,
-    //   onCloseElement,
-    //   <ContentbarExampleWithText exampleText="car129" />
-    // ),
+    new BasicContentbarWrapper(
+      'test123',
+      'car123',
+      selectedId,
+      false,
+      setSelectedId,
+      onCloseElement,
+      <ContentbarExampleWithText exampleText="car123" />
+    ),
+    new BasicContentbarWrapper(
+      'test124',
+      'car124',
+      selectedId,
+      false,
+      setSelectedId,
+      onCloseElement,
+      <ContentbarExampleWithText exampleText="car124" />
+    ),
+    new BasicContentbarWrapper(
+      'test125',
+      'car125',
+      selectedId,
+      true,
+      setSelectedId,
+      onCloseElement,
+      <ContentbarExampleWithText exampleText="car125" />
+    ),
+    new BasicContentbarWrapper(
+      'test126',
+      'car126',
+      selectedId,
+      true,
+      setSelectedId,
+      onCloseElement,
+      <ContentbarExampleWithText exampleText="car126" />
+    ),
+    new BasicContentbarWrapper(
+      'test127',
+      'car127',
+      selectedId,
+      true,
+      setSelectedId,
+      onCloseElement,
+      <ContentbarExampleWithText exampleText="car127" />
+    ),
+    new BasicContentbarWrapper(
+      'test128',
+      'car128',
+      selectedId,
+      true,
+      setSelectedId,
+      onCloseElement,
+      <ContentbarExampleWithText exampleText="car128" />
+    ),
+    new BasicContentbarWrapper(
+      'test129',
+      'car129',
+      selectedId,
+      true,
+      setSelectedId,
+      onCloseElement,
+      <ContentbarExampleWithText exampleText="car129" />
+    ),
   ];
 
   const selectElement = (value: string) => {
@@ -103,14 +105,14 @@ export const ExampleComponent1 = () => {
       onClickRightSlideButton={test4}
       setSelectedId={selectElement}
       layoutBehaviour={LayoutBehaviour.GRID}
-      contentElements={[]}
+      contentElements={exampleArray}
     >
       <>
-        {/* {exampleArray.map((basicContentbarWrapper: BasicContentbarWrapper) => {
+        {exampleArray.map((basicContentbarWrapper: BasicContentbarWrapper) => {
           if (basicContentbarWrapper.getId() === selectedId) {
             return basicContentbarWrapper.getContentAreaElement();
           }
-        })} */}
+        })}
       </>
     </Content>
   );

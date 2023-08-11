2b689f2af94bb744823ec2196369e23d763df7de
Jörg Jonas Krahn <joerg.jonas.krahn@iav.de>
2023-08-11 13:09:54 +0200

    test

--- scripts/ci_cd/configure_npmrc_for_publish.sh
+++ scripts/ci_cd/configure_npmrc_for_publish.sh
@@ -1,3 +1,3 @@
 # Write a new line and the authentication information to the .npmrc file.
 echo $'\n' >> .npmrc
-curl -s -u$artifactory_user:$artifactory_apikey https://artifactory.iav.com/artifactory/api/npm/npm-repo/auth/@npm-iav-frontend | awk '{printf "//artifactory.iav.com/artifactory/:%s\n", $0}' >> .npmrc
+curl -s -u$artifactory_user:$artifactory_apikey https://artifactory.iav.com/artifactory/api/npm/npm-repo/auth/npm-iav-frontend | awk '{printf "//artifactory.iav.com/artifactory/:%s\n", $0}' >> .npmrc

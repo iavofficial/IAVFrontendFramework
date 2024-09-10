33e411565dacfd2326069ff29765c8542da5a108
mikewegele <mike.wegele@iav.de>
2024-09-10 17:43:43 +0200

    TTT5Framework-190: Add Doku to HTML

---
+++ pages/script/nav.js
@@ -0,0 +1,18 @@
+function loadNav() {
+  fetch('./html/nav.html')
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.getElementById('nav-placeholder').innerHTML = data;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', loadNav);
+
+function toggleDrawer() {
+  const drawer = document.getElementById('drawer');
+  if (drawer) {
+    drawer.classList.toggle('open');
+  }
+}

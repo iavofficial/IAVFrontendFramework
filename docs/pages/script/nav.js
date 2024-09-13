4b93a059f9df44c47d64dc7a05339c16f1c15956
mikewegele <mike.wegele@iav.de>
2024-09-13 18:03:47 +0200

    feature: add imprint

--- docs/pages/script/nav.js
+++ docs/pages/script/nav.js
@@ -1,32 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('./pages/html/nav.html')
-    .then(response => {
-      return response.text()
-    })
-    .then(data => {
-      document.getElementById('nav-placeholder').innerHTML = data;
-    });
+    fetch('./pages/html/nav.html')
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.getElementById('nav-placeholder').innerHTML = data;
+        });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('./pages/html/header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('header-container').innerHTML = data;
-    });
+    fetch('./pages/html/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('./pages/html/imprint-footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('./pages/html/nav.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('drawer').innerHTML = data;
-      const currentPath = window.location.pathname.split('/').pop();
-      const links = document.querySelectorAll('.drawer a');
-      links.forEach(link => {
-        if (link.getAttribute('href') === currentPath) {
-          link.classList.add('active');
-        }
-      });
-    });
+    fetch('./pages/html/nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('drawer').innerHTML = data;
+            const currentPath = window.location.pathname.split('/').pop();
+            const links = document.querySelectorAll('.drawer a');
+            links.forEach(link => {
+                if (link.getAttribute('href') === currentPath) {
+                    link.classList.add('active');
+                }
+            });
+        });
 });

3d72007ad31145226aa01d155048b9f41debd035
mikewegele <mike.wegele@iav.de>
2025-01-20 13:00:50 +0100

    add errorMessage as prop to authenticationViewProps.ts

--- src/components/authentication/authenticationViewProps.ts
+++ src/components/authentication/authenticationViewProps.ts
@@ -17,19 +17,23 @@
  */
 
 import {ReactElement} from "react";
+
 export interface AuthenticationViewProps {
-  authOptions?: {
-    backgroundImage?: string;
-    companyText?: string;
-    documentsLabelKey?: string;
-    preventDarkmode?: boolean;
-  };
-  headerOptions?: {
-    reactElementLeft?: ReactElement;
-    reactElementRight?: ReactElement;
-    hideLeft?: boolean;
-    hideRight?: boolean;
-  };
-  hideLanguageSelection?: boolean;
-  hideLegalDocuments?: boolean;
+    authOptions?: {
+        backgroundImage?: string;
+        companyText?: string;
+        documentsLabelKey?: string;
+        preventDarkmode?: boolean;
+    };
+    headerOptions?: {
+        reactElementLeft?: ReactElement;
+        reactElementRight?: ReactElement;
+        hideLeft?: boolean;
+        hideRight?: boolean;
+    };
+    hideLanguageSelection?: boolean;
+    hideLegalDocuments?: boolean;
+    errorMessages?: {
+        passwordErrorMessage?: string;
+    }
 }

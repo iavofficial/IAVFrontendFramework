d41f3cc8ce64554fc9f70acfa09606bd56f72d86
mikewegele <mike.wegele@iav.de>
2025-01-31 09:41:53 +0100

    npm run format

--- src/components/cookie/cookieHooks.ts
+++ src/components/cookie/cookieHooks.ts
@@ -21,9 +21,10 @@ import {useCookies} from "react-cookie";
 import {ACCEPTED_COOKIES_NAME} from "../../constants";
 
 export function useCookiesAccepted() {
-    const [cookies] = useCookies([ACCEPTED_COOKIES_NAME]);
-    return (
-        cookies[ACCEPTED_COOKIES_NAME] !== undefined &&
-        (cookies[ACCEPTED_COOKIES_NAME] === "true" || cookies[ACCEPTED_COOKIES_NAME] === true)
-    );
+  const [cookies] = useCookies([ACCEPTED_COOKIES_NAME]);
+  return (
+    cookies[ACCEPTED_COOKIES_NAME] !== undefined &&
+    (cookies[ACCEPTED_COOKIES_NAME] === "true" ||
+      cookies[ACCEPTED_COOKIES_NAME] === true)
+  );
 }

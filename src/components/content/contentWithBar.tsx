c745b80131d38446cac28aff84b68af94d8139ce
mikewegele <mike.wegele@iav.de>
2025-02-20 16:20:24 +0100

    fix: content with bar

--- src/components/content/contentWithBar.tsx
+++ src/components/content/contentWithBar.tsx
@@ -18,84 +18,75 @@
 
 import React, {useMemo} from "react";
 import "../css/globalColors.css";
-import {
-  ContentBar,
-  ContentBarStyles,
-  ContentBarStylesArray,
-} from "./contentBar";
+import {ContentBar, ContentBarStyles, ContentBarStylesArray,} from "./contentBar";
 import {BasicContentbarWrapper} from "./basicContentbarWrapper";
 import {CustomContentbarWrapper} from "./customContentbarWrapper";
 import {ContentLayout, ContentLayoutAndStyleProps} from "./contentLayout";
 
 export type ContentWithBarProps = {
-  contentWrappers: BasicContentbarWrapper[] | CustomContentbarWrapper[];
-  selectedId: string;
-  addable?: boolean;
-  jumpToEndOfContentBar?: boolean;
-  onClickAddButton?: () => any;
-  onClickLeftSlideButton?: () => any;
-  onClickRightSlideButton?: () => any;
+    contentWrappers: BasicContentbarWrapper[] | CustomContentbarWrapper[];
+    selectedId: string;
+    addable?: boolean;
+    jumpToEndOfContentBar?: boolean;
+    onClickAddButton?: () => any;
+    onClickLeftSlideButton?: () => any;
+    onClickRightSlideButton?: () => any;
 };
 
 export type ContentLayoutAndStyleAndWithBarProps = ContentLayoutAndStyleProps &
-  ContentWithBarProps;
+    ContentWithBarProps;
 
 export const ContentWithBar = (
-  props: React.PropsWithChildren<ContentLayoutAndStyleAndWithBarProps>,
+    props: React.PropsWithChildren<ContentLayoutAndStyleAndWithBarProps>,
 ) => {
-  const contentBarStyles = useMemo(() => {
-    const tempContentbarStyles: ContentBarStylesArray = [];
-    Object.values(ContentBarStyles).forEach((contentBarStyle) => {
-      if (props.contentStyle?.appliedStyles?.includes(contentBarStyle)) {
-        tempContentbarStyles.push(contentBarStyle);
-        if (contentBarStyle === ContentBarStyles.SET_SPACING_COLOR) {
-          tempContentbarStyles.push(ContentBarStyles.SPACING);
-        }
-      }
-    });
-    return tempContentbarStyles;
-  }, [props.contentStyle]);
+    const selectedContentWrapper = useMemo(() => {
+        return props.contentWrappers.find(
+            (currentWrapper) => currentWrapper.getId() === props.selectedId,
+        );
+    }, [props.contentWrappers, props.selectedId]);
 
-  return (
-    <div className="flex flex-column" style={{width: "100%", overflow: "auto"}}>
-      {props.contentWrappers.length >= 1 && (
-        <ContentBar
-          selectedId={props.selectedId}
-          onClickLeftSlideButton={props.onClickLeftSlideButton}
-          onClickRightSlideButton={props.onClickRightSlideButton}
-          onClickAddButton={props.onClickAddButton}
-          addable={props.addable}
-          jumpToEndOfContentBar={props.jumpToEndOfContentBar}
-          contentElements={props.contentWrappers}
-          appliedStyles={contentBarStyles}
-        />
-      )}
+    const contentBarStyles = useMemo(() => {
+        const tempContentbarStyles: ContentBarStylesArray = [];
+        Object.values(ContentBarStyles).forEach((contentBarStyle) => {
+            if (props.contentStyle?.appliedStyles?.includes(contentBarStyle)) {
+                tempContentbarStyles.push(contentBarStyle);
+                if (contentBarStyle === ContentBarStyles.SET_SPACING_COLOR) {
+                    tempContentbarStyles.push(ContentBarStyles.SPACING);
+                }
+            }
+        });
+        return tempContentbarStyles;
+    }, [props.contentStyle]);
 
-      <div
-        className="w-full"
-        style={{
-          height: "100%",
-          overflow: "auto",
-        }}
-      >
-        <ContentLayout
-          layoutBehaviour={props.layoutBehaviour}
-          contentStyle={props.contentStyle}
-        >
-          {props.contentWrappers.map((tab) => (
+    return (
+        <div className="flex flex-column" style={{width: "100%", overflow: "auto"}}>
+            {props.contentWrappers.length >= 1 && (
+                <ContentBar
+                    selectedId={props.selectedId}
+                    onClickLeftSlideButton={props.onClickLeftSlideButton}
+                    onClickRightSlideButton={props.onClickRightSlideButton}
+                    onClickAddButton={props.onClickAddButton}
+                    addable={props.addable}
+                    jumpToEndOfContentBar={props.jumpToEndOfContentBar}
+                    contentElements={props.contentWrappers}
+                    appliedStyles={contentBarStyles}
+                />
+            )}
+
             <div
-              key={tab.getId()}
-              style={{
-                height: "100%",
-                width: "100%",
-                display: props.selectedId === tab.getId() ? "flex" : "none",
-              }}
+                className="w-full"
+                style={{
+                    height: "100%",
+                    overflow: "auto",
+                }}
             >
-              {tab.getContentAreaElement()}
+                <ContentLayout
+                    layoutBehaviour={props.layoutBehaviour}
+                    contentStyle={props.contentStyle}
+                >
+                    {selectedContentWrapper?.getContentAreaElement()}
+                </ContentLayout>
             </div>
-          ))}
-        </ContentLayout>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };

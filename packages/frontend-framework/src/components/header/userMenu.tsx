3d14951d0b2506c8fcb7cd8ae68c56d973d8509e
mikewegele <mike.wegele@iav.de>
2025-03-31 10:23:10 +0200

    chore: userMenu.tsx back to old state

--- packages/frontend-framework/src/components/header/userMenu.tsx
+++ packages/frontend-framework/src/components/header/userMenu.tsx
@@ -21,7 +21,7 @@ import {ContextMenu} from "primereact/contextmenu";
 import {MenuItem} from "./settingsMenu";
 import {useModule} from "@iavofficial/frontend-framework-shared/moduleContext";
 import {useDefaultDispatch} from "@iavofficial/frontend-framework-shared/moduleDefaults";
-import {MandatoryModuleNames} from "@iavofficial/frontend-framework/constants";
+import {MandatoryModuleNames} from "@iavofficial/frontend-framework-shared/moduleNames";
 
 export interface Props {
     hideMenu: (e: React.KeyboardEvent) => void;

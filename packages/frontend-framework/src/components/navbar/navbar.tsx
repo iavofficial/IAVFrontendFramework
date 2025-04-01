f1179d36035393b5bc78e88b782ec408f60bb7b9
mikewegele <mike.wegele@iav.de>
2025-04-01 15:53:43 +0200

    fix: height inherit for navbar.tsx

--- packages/frontend-framework/src/components/navbar/navbar.tsx
+++ packages/frontend-framework/src/components/navbar/navbar.tsx
@@ -19,16 +19,15 @@
 import React, {useContext} from "react";
 import "./navbar.css";
 import {TabAndContentWrapper} from "./wrappers/typesWrappers";
-import {Tooltip} from "primereact/tooltip";
 import SimpleBar from "simplebar-react";
 import "simplebar-react/dist/simplebar.min.css";
 import {calculateNavbarArrowFunctionColor} from "../../utils/calculateNavbarArrowColor";
 import {NavbarSettingsContext} from "../../contexts/navbarContext";
 import {
-  DEFAULT_ELEMENT_SIZE,
-  GAB_NAVBAR_COLLAPSED,
-  NAVBAR_WIDTH_UNFOLDED,
-  PADDING_GAB,
+    DEFAULT_ELEMENT_SIZE,
+    GAB_NAVBAR_COLLAPSED,
+    NAVBAR_WIDTH_UNFOLDED,
+    PADDING_GAB,
 } from "@iavofficial/frontend-framework-shared/constants";
 import {ColorSettingsContext} from "@iavofficial/frontend-framework-shared/colorSettingsContext";
 import {generateHashOfLength} from "@iavofficial/frontend-framework-shared/hash";
@@ -37,139 +36,139 @@ import {MandatoryModuleNames} from "@iavofficial/frontend-framework-shared/modul
 import {useModuleTranslation} from "@iavofficial/frontend-framework-shared/useModuleTranslation";
 
 interface Props {
-  tabAndContentWrappers: TabAndContentWrapper[];
-  documentsLabelKey?: string;
-  hideImprint?: boolean;
-  hidePrivacyPolicy?: boolean;
+    tabAndContentWrappers: TabAndContentWrapper[];
+    documentsLabelKey?: string;
+    hideImprint?: boolean;
+    hidePrivacyPolicy?: boolean;
 }
 
 export const Navbar = (props: Props) => {
-  const t = useModuleTranslation();
+    const t = useModuleTranslation();
 
-  const routerModule = useModule(MandatoryModuleNames.Router);
-  const Link = routerModule.Link;
+    const routerModule = useModule(MandatoryModuleNames.Router);
+    const Link = routerModule.Link;
 
-  const colorSettingsContext = useContext(ColorSettingsContext);
-  const navbarSettingsContext = useContext(NavbarSettingsContext);
+    const colorSettingsContext = useContext(ColorSettingsContext);
+    const navbarSettingsContext = useContext(NavbarSettingsContext);
 
-  const navbarColor = colorSettingsContext.currentColors.navbar.backgroundColor;
+    const navbarColor = colorSettingsContext.currentColors.navbar.backgroundColor;
 
-  const legalDocumentsColor =
-    colorSettingsContext.currentColors.navbar.legalDocumentsLinkColor;
+    const legalDocumentsColor =
+        colorSettingsContext.currentColors.navbar.legalDocumentsLinkColor;
 
-  const navbarCollapseArrowColor =
-    colorSettingsContext.currentColors.navbar.navbarCollapseArrowColor;
+    const navbarCollapseArrowColor =
+        colorSettingsContext.currentColors.navbar.navbarCollapseArrowColor;
 
-  const scrollbarColor =
-    colorSettingsContext.currentColors.navbar.scrollbarColor;
+    const scrollbarColor =
+        colorSettingsContext.currentColors.navbar.scrollbarColor;
 
-  const identifier = generateHashOfLength(4);
-  const identifierLegal = "a" + identifier;
-  const identifierWithDot = "." + identifierLegal;
+    const identifier = generateHashOfLength(4);
+    const identifierLegal = "a" + identifier;
+    const identifierWithDot = "." + identifierLegal;
 
-  return (
-    <div className="h-full" style={{backgroundColor: navbarColor}}>
-      <div id="navbar" className="h-full">
-        <SimpleBar
-          style={{
-            height: "inherit"m
-            width: navbarSettingsContext.navbarCollapsed
-              ? `${DEFAULT_ELEMENT_SIZE + 2 * GAB_NAVBAR_COLLAPSED}px`
-              : `${NAVBAR_WIDTH_UNFOLDED}px`,
-            padding: navbarSettingsContext.navbarCollapsed
-              ? "0px 2px 0px 2px"
-              : "0px 4px 0px 4px",
-            color: scrollbarColor,
-            position: "relative",
-            overflowX: "visible",
-            marginBottom: "30px",
-            flex: "0 1 auto",
-          }}
-          className="custom-scrollbar"
-        >
-          <>
-            {props.tabAndContentWrappers.map((wrapper: TabAndContentWrapper) =>
-              wrapper.getNavbarComponent({
-                navbarCollapsed: navbarSettingsContext.navbarCollapsed,
-              }),
-            )}
-          </>
-        </SimpleBar>
-        <div
-          id="navbar-bottom-wrapper"
-          className={"text-center flex "}
-          style={
-            navbarSettingsContext.navbarCollapsed
-              ? {
-                  justifyContent: "center",
-                  flexDirection: "column",
-                  width: "44px",
-                  gap: "10px",
-                }
-              : {
-                  justifyContent: "center",
-                }
-          }
-        >
-          {(props.hideImprint === true && props.hidePrivacyPolicy === true) ===
-            false && (
-            <div
-              id="legal-doc-links"
-              style={{
-                flexDirection: navbarSettingsContext.navbarCollapsed
-                  ? "unset"
-                  : "row",
-                writingMode: navbarSettingsContext.navbarCollapsed
-                  ? "sideways-lr"
-                  : "horizontal-tb",
-              }}
-            >
-              {!props.hideImprint && (
-                <Link
-                  className="legal-doc-link"
-                  style={{color: legalDocumentsColor}}
-                  to="/imprint"
+    return (
+        <div className="h-full" style={{backgroundColor: navbarColor}}>
+            <div id="navbar" className="h-full">
+                <SimpleBar
+                    style={{
+                        height: "inherit",
+                        width: navbarSettingsContext.navbarCollapsed
+                            ? `${DEFAULT_ELEMENT_SIZE + 2 * GAB_NAVBAR_COLLAPSED}px`
+                            : `${NAVBAR_WIDTH_UNFOLDED}px`,
+                        padding: navbarSettingsContext.navbarCollapsed
+                            ? "0px 2px 0px 2px"
+                            : "0px 4px 0px 4px",
+                        color: scrollbarColor,
+                        position: "relative",
+                        overflowX: "visible",
+                        marginBottom: "30px",
+                        flex: "0 1 auto",
+                    }}
+                    className="custom-scrollbar"
                 >
-                  {t({key: "Imprint"})}
-                </Link>
-              )}
-              {!props.hidePrivacyPolicy && (
-                <Link
-                  className="legal-doc-link"
-                  style={{color: legalDocumentsColor}}
-                  to="/privacy-policy"
+                    <>
+                        {props.tabAndContentWrappers.map((wrapper: TabAndContentWrapper) =>
+                            wrapper.getNavbarComponent({
+                                navbarCollapsed: navbarSettingsContext.navbarCollapsed,
+                            }),
+                        )}
+                    </>
+                </SimpleBar>
+                <div
+                    id="navbar-bottom-wrapper"
+                    className={"text-center flex "}
+                    style={
+                        navbarSettingsContext.navbarCollapsed
+                            ? {
+                                justifyContent: "center",
+                                flexDirection: "column",
+                                width: "44px",
+                                gap: "10px",
+                            }
+                            : {
+                                justifyContent: "center",
+                            }
+                    }
                 >
-                  {t({key: "Privacy_Policy"})}
-                </Link>
-              )}
+                    {(props.hideImprint === true && props.hidePrivacyPolicy === true) ===
+                        false && (
+                            <div
+                                id="legal-doc-links"
+                                style={{
+                                    flexDirection: navbarSettingsContext.navbarCollapsed
+                                        ? "unset"
+                                        : "row",
+                                    writingMode: navbarSettingsContext.navbarCollapsed
+                                        ? "sideways-lr"
+                                        : "horizontal-tb",
+                                }}
+                            >
+                                {!props.hideImprint && (
+                                    <Link
+                                        className="legal-doc-link"
+                                        style={{color: legalDocumentsColor}}
+                                        to="/imprint"
+                                    >
+                                        {t({key: "Imprint"})}
+                                    </Link>
+                                )}
+                                {!props.hidePrivacyPolicy && (
+                                    <Link
+                                        className="legal-doc-link"
+                                        style={{color: legalDocumentsColor}}
+                                        to="/privacy-policy"
+                                    >
+                                        {t({key: "Privacy_Policy"})}
+                                    </Link>
+                                )}
+                            </div>
+                        )}
+
+                    {navbarSettingsContext.collapsible && (
+                        <i
+                            onClick={() =>
+                                navbarSettingsContext.setNavbarCollapsed(
+                                    !navbarSettingsContext.navbarCollapsed,
+                                )
+                            }
+                            style={{
+                                ...(!navbarSettingsContext.navbarCollapsed && {
+                                    position: "absolute",
+                                    right: 0,
+                                }),
+                                cursor: "pointer",
+                                color: navbarCollapseArrowColor,
+                                margin: navbarSettingsContext.navbarCollapsed
+                                    ? "8px 0px 0px 0px"
+                                    : `0px ${PADDING_GAB}px 0px 0px`,
+                            }}
+                            className={calculateNavbarArrowFunctionColor(
+                                navbarSettingsContext.navbarCollapsed!,
+                            )}
+                        />
+                    )}
+                </div>
             </div>
-          )}
-
-          {navbarSettingsContext.collapsible && (
-            <i
-              onClick={() =>
-                navbarSettingsContext.setNavbarCollapsed(
-                  !navbarSettingsContext.navbarCollapsed,
-                )
-              }
-              style={{
-                ...(!navbarSettingsContext.navbarCollapsed && {
-                  position: "absolute",
-                  right: 0,
-                }),
-                cursor: "pointer",
-                color: navbarCollapseArrowColor,
-                margin: navbarSettingsContext.navbarCollapsed
-                  ? "8px 0px 0px 0px"
-                  : `0px ${PADDING_GAB}px 0px 0px`,
-              }}
-              className={calculateNavbarArrowFunctionColor(
-                navbarSettingsContext.navbarCollapsed!,
-              )}
-            />
-          )}
         </div>
-      </div>
-    </div>
-  );
+    );
 };

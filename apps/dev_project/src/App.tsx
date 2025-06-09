b0c1409ac747ef7df6d97daeb0d78027913eb4c7
mikewegele <mike.wegele@iav.de>
2025-06-09 07:38:20 +0200

    chore: npm run format

--- apps/dev_project/src/App.tsx
+++ apps/dev_project/src/App.tsx
@@ -17,41 +17,41 @@
  */
 
 import translationES from "./assets/translations/es.json";
-import {GlobalDataLayer} from "@iavofficial/frontend-framework/globalDataLayer";
-import {DummyAuthenticationProvider} from "@iavofficial/frontend-framework/dummyAuthenticationProvider";
+import { GlobalDataLayer } from "@iavofficial/frontend-framework/globalDataLayer";
+import { DummyAuthenticationProvider } from "@iavofficial/frontend-framework/dummyAuthenticationProvider";
 import translationEN from "./assets/translations/en.json";
 import translationDE from "./assets/translations/de.json";
 import translationDECH from "./assets/translations/de-CH.json";
 import Layout from "./Layout.tsx";
 
 function App() {
-    const translations = {
-        es: {
-            translation: translationES,
-        },
-        en: {
-            translation: translationEN,
-        },
-        de: {
-            translation: translationDE,
-        },
-        de_CH: {
-            translation: translationDECH,
-        },
-    };
+  const translations = {
+    es: {
+      translation: translationES,
+    },
+    en: {
+      translation: translationEN,
+    },
+    de: {
+      translation: translationDE,
+    },
+    de_CH: {
+      translation: translationDECH,
+    },
+  };
 
-    return (
-        <DummyAuthenticationProvider>
-            <GlobalDataLayer
-                translations={translations}
-                colorSettings={{
-                    colorOptions: {},
-                }}
-            >
-                <Layout/>
-            </GlobalDataLayer>
-        </DummyAuthenticationProvider>
-    );
+  return (
+    <DummyAuthenticationProvider>
+      <GlobalDataLayer
+        translations={translations}
+        colorSettings={{
+          colorOptions: {},
+        }}
+      >
+        <Layout />
+      </GlobalDataLayer>
+    </DummyAuthenticationProvider>
+  );
 }
 
 export default App;

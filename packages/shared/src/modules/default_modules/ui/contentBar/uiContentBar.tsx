aeeae8f30571c3cf42c90535245db351ca1ca79d
mikewegele <mike.wegele@iav.de>
2025-10-13 07:35:21 +0200

    refactor: add spdx licences

--- packages/shared/src/modules/default_modules/ui/contentBar/uiContentBar.tsx
+++ packages/shared/src/modules/default_modules/ui/contentBar/uiContentBar.tsx
@@ -1,6 +1,19 @@
 /**
- * Copyright © 2025 IAV GmbH Ingenieurgesellschaft Auto und Verkehr
- * Apache-2.0
+ * Copyright © 2025 IAV GmbH Ingenieurgesellschaft Auto und Verkehr, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 import React, {
@@ -97,14 +110,14 @@ export const UIContentBar: React.FC<UIContentBarProps> = (props) => {
       setStartRenderElements(handleJumpToEnd);
       setPreventInitialJumpToEnd(false);
     }
-  }, [contentElements.length]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [contentElements.length]);
 
   useEffect(() => {
     window.addEventListener("resize", handleDivResize);
     return () => {
       window.removeEventListener("resize", handleDivResize);
     };
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
   const lastElementIsVisible = useCallback(() => {
     return (
--- packages/shared/src/types/modules/ui/cookieBanner/cookieBannerModuleInterfaces.ts
+++ packages/shared/src/types/modules/ui/cookieBanner/cookieBannerModuleInterfaces.ts
@@ -15,6 +15,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
 import React from "react";
 
 export interface UICookieBannerProps {

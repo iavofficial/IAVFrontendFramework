fa110c9a19e6f577963a802bc2ba421413c38942
Helfenstein <daniel.helfenstein@iav.de>
2025-12-01 12:33:33 +0100

    fix: change to aws-amplify/auth imports; edit babel preset

--- apps/dev_project/src/index.tsx
+++ apps/dev_project/src/index.tsx
@@ -25,5 +25,5 @@ import AppUI from "./components/ui_test/AppUI.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppDefaultAuthentication />
-  </React.StrictMode>,
+  </React.StrictMode>
 );

c513654f6731bc01819568c6baab0082a6c6d57a
Jonas Krahn <joerg.jonas.krahn@iav.de>
2025-12-02 13:36:30 +0100

    Update moduleHelperTypes.ts

--- packages/shared/src/types/modules/moduleHelperTypes.ts
+++ packages/shared/src/types/modules/moduleHelperTypes.ts
@@ -2,8 +2,13 @@ import type { TypedUseSelectorHook } from "react-redux";
 import type { Action, ThunkDispatch } from "@reduxjs/toolkit";
 import type { RootState } from "./moduleOrchestrationTypes";
 
-/** Use this hook for correct typing with redux selectors. */
-export type TypedSelectorHook<T extends (...args: any) => any> = TypedUseSelectorHook<RootState<T>>;
+type GeneralFunction = (...args: any) => any;
+
+/** 
+ * Use this hook for correct typing with redux selectors.
+ *  */
+export type TypedSelectorHook<S extends GeneralFunction> = TypedUseSelectorHook<RootState<S>>;
 
 /** Use this hook for correct typing with redux dispatches. */
-export type TypedDispatchHook<T extends (...args: any) => any> = ThunkDispatch<RootState<T>, unknown, Action<string>>;
+export type TypedDispatchHook<S extends GeneralFunction> =
+ThunkDispatch<ReturnType<S>, unknown, Action<string>>;

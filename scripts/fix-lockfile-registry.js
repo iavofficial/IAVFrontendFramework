b80183d6827e3f64bc94d00a6480a2056f5eae71
Jonas Krahn <joerg.jonas.krahn@iav.de>
2026-08-28 10:36:49 +0200

    chore: Fixed build pipeline

--- scripts/fix-lockfile-registry.js
+++ scripts/fix-lockfile-registry.js
@@ -1,11 +1,22 @@
 const fs = require("fs");
 
 const lockfile = "package-lock.json";
-const artifactoryHost = "https://artifactory.iav.com";
-const npmjs = "https://registry.npmjs.org";
+const npmjs = "https://registry.npmjs.org/";
 
 let content = fs.readFileSync(lockfile, 'utf8');
 
-content = content.replace(new RegExp(artifactoryHost.replace(/\//g, "\\/"), "g"), npmjs);
+content = content
+  .replace(
+    /https:\/\/artifactory\.iav\.com\/artifactory\/api\/npm\/npm\//g,
+    npmjs,
+  )
+  .replace(
+    /https:\/\/registry\.npmjs\.org\/artifactory\/api\/npm\/npm\//g,
+    npmjs,
+  )
+  .replace(
+    /https:\/\/portal\.partner\.iavtech\.net\/F5Networks-SSO-Req[^\s"]*/g,
+    npmjs,
+  );
 
 fs.writeFileSync(lockfile, content);
